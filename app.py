8e257385f1f9f386b16073282a198dbefb9cc6a1
Stanley Chin <stanchin@ca.ibm.com>
2016-06-30 22:36:16 -0400

    added reroll, cleaned up UI, handled error cases

--- app.py
+++ app.py
@@ -5,25 +5,50 @@ from flask import Flask, render_template, request
 from Show import Episode
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+
+#global variables#######
+allEpisodes = []
+#global variables#######
 
 @app.route('/')
 def main():
 	return render_template('query.html')
 
 @app.route('/', methods=['POST'])
-def query_console(showname=None, name=None, season=None, episode=None):
-	_title = request.form['title']
-	_url = getURL(_title)
+def query_console(placeholder=None, showname=None, name=None, season=None, episode=None):
 
-	r = requests.get(_url)
-	ep = getData(r)
+	try: 
+		_title = request.form['title']
+		if not _title:
+			return render_template('query.html', placeholder="Input cannot be empty")
+			
+		_url = getURL(_title)
 
+		r = requests.get(_url)
+		ep = getData(r)
+
+		showname = ep.showname
+		name = ep.name
+		season = ep.seasonNum
+		episode = ep.episodenum
+
+	except ValueError as e:
+		print(e)
+		return render_template('query.html', placeholder="Please enter a proper TV show name")
+
+	return render_template('result.html', showname=showname, name=name, season=season, episode=episode)
+
+@app.route('/reroll', methods=['GET'])
+def reroll(showname=None, name=None, season=None, episode=None):
+	
+	ep = randomize()
 	showname = ep.showname
 	name = ep.name
 	season = ep.seasonNum
 	episode = ep.episodenum
-
 	return render_template('result.html', showname=showname, name=name, season=season, episode=episode)
+
 
 def getURL(title):
 	title = addSpace(title)
@@ -39,7 +64,7 @@ def getData(response):
 		alleps = result["_embedded"]["episodes"]
 		episodes = []
 
-		for a in alleps: #for each episode
+		for a in alleps: 				#for each episode
 			epName = a["name"] 			#get name
 			seasonNum = a["season"] 	#get season
 			epNum = a["number"]			#get episode number
@@ -47,12 +72,20 @@ def getData(response):
 			episode = Episode(epName, seasonNum, epNum, result["name"]) 	#make an Episode obj
 			episodes.append(episode)
 
-		randomEp = randint(0, len(episodes)-1)
-		getEp = episodes[randomEp]
-		return getEp
+
+		# Episodes list generated 
+		global allEpisodes
+		allEpisodes = episodes 
+
+		return randomize()
 
 	except ValueError as e:
 		print(e)
+
+def randomize():
+	randomEp = randint(0, len(allEpisodes)-1)
+	ep = allEpisodes[randomEp]
+	return ep
 
 def addSpace(title):
 	newTitle=""
